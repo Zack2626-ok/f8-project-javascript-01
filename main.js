66c472985557a0bea4c20137fe2b698d6b6a6b68
Zack2626-ok <thezach2626@gmail.com>
2025-05-24 09:23:03 +0700

    Fix và tối ưu Duplicate code và Lưu giữ liệu vào localstorate

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
-const tasks = [];
+const tasks = JSON.parse(localStorage.getItem("task")) ?? [];
 
 const taskList = document.getElementById('task-list');
 const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('#todo-input');
 
+
+// Local Storage
+// JSON
+
+function saveTask() {
+    localStorage.setItem('task', JSON.stringify(tasks));
+}
+
+
+
+function isDuplicateTask(newTitle,  excludeIndex = -1) {
+    const isDuplicate = tasks.some((task,index) => excludeIndex !== index && task.title.toLowerCase() === newTitle.toLowerCase());
+    return isDuplicate;
+}
+
 function handleTaskAction(e) {
     const taskItem = e.target.closest('.task-item');
     const taskIndex = +taskItem.getAttribute('task-index');
     const task = tasks[taskIndex];
-
+    
     if(e.target.closest('.edit')){
-        const newTitle = prompt('Enter new title: ', task.title);
+        let newTitle = prompt('Enter new title: ', task.title);
+        
+        if(newTitle === null) return;
+
+        newTitle = newTitle.trim();
+        if(!newTitle){
+            alert('Task title cannot be empty!!!');
+            return;
+        }
+        
+        if(isDuplicateTask(newTitle, taskIndex)){
+            alert('Task title already exists!!!');
+            return;
+        }
+        
         task.title = newTitle;
         renderTask();
+        saveTask();
     } else if (e.target.closest('.done')){
         task.completed = !task.completed;
         renderTask();
+        saveTask();
     } else if (e.target.closest('.delete')){
         if (confirm(`Are you sure you want to delete "${task.title}" ?`)){
             tasks.splice(taskIndex, 1);
             renderTask();
+            saveTask();
         }
         
     }
@@ -31,17 +63,30 @@ function addTask(e) {
     const value = todoInput.value.trim();
 
     if(!value) return alert('Please write something!!!');
+
+    // const isDuplicate = tasks.some(task => task.title.toLowerCase() === value.toLowerCase());
+    if(isDuplicateTask(value)){
+        return alert('Task aready exists!!!');
+    }
+
+
     
     tasks.push({
         title: value,
         completed: false
     });
     renderTask();
+    saveTask()
     todoInput.value = '';
 }
 
 
 function renderTask() {
+    if(!tasks.length){
+        taskList.innerHTML = '<li class="emty-message">No tasks available</li>';
+        return;
+    }
+
     const html = tasks.map((task, index) => {
         return `<li class="task-item ${task.completed ? "completed" : ""}" task-index="${index} ">
             <span class="task-title">${task.title}</span>
@@ -53,7 +98,7 @@ function renderTask() {
         </li>`
     }).join('');
     
-    taskList.innerHTML = html
+    taskList.innerHTML = html;
 }
 
 todoForm.addEventListener('submit', addTask)
